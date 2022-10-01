07a0a3613baa90255069707253dd6ee4a92721e6
julius-m20 <juliusmarkwei2000@gmail.com>
2022-10-01 00:21:26 +0000

    changes

---
+++ Main/Menu.java
@@ -0,0 +1,23 @@
+public class Group8{
+    int numberOfElements, key;
+    int element[];
+    int firstDecision, secondDecision;
+
+    public static void header(){
+        System.out.println("\n\t\t*****************************************************************************");
+        System.out.println("\n\t\t\t\t\tSEARCHING AND SORTING ALGORITHMS");
+        System.out.println("\n\t\t*****************************************************************************");
+        System.out.println("\n");
+    }
+
+    public static void welcomeMessage(){
+        System.out.println("The Algorithms available in this Program are:");
+        System.out.println("Searching Algorithms");
+        System.out.println("Sorting Algorithms");
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+
+    }
+};
